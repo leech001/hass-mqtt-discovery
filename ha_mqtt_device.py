b3e7841f377788b13a1327293be6258f0d49c3ea
Francois Gervais <francoisgervais@gmail.com>
2021-10-10 11:51:11 -0400

    Transition Sensor to using Device and being a Component

--- ha_mqtt_device.py
+++ ha_mqtt_device.py
@@ -4,6 +4,8 @@ import yaml
 import paho.mqtt.client as mqtt
 
 
+DISCOVERY_PREFIX = "homeassistant"
+
 
 class Device(dict):
     def __init__(self, name, sw_version, model, manufacturer):
@@ -24,26 +26,51 @@ class Device(dict):
             return device
 
 
-class Sensor:
-    def __init__(self, client: mqtt.Client, name, unit_of_measurement, icon):
+class Component:
+    def __init__(self, name):
+        self.component = name
+
+
+class Sensor(Component):
+    def __init__(
+        self,
+        client: mqtt.Client,
+        name,
+        parent_device,
+        unit_of_measurement,
+        icon=None,
+        topic_parent_level="",
+    ):
+        super().__init__("sensor")
+
         self.client = client
         self.name = name
-        self.unique_id = self.name.replace(" ", "_").lower()
+        self.parent_device = parent_device
+        self.object_id = self.name.replace(" ", "_").lower()
         self.unit_of_measurement = unit_of_measurement
         self.icon = icon
-        self.topic = f"homeassistant/sensor/{self.unique_id}"
+        self.topic_parent_level = topic_parent_level
+        self.topic = f"{self.parent_device.name}/{self.component}/{self.topic_parent_level}/{self.object_id}"
         self._send_config()
 
     def _send_config(self):
         _config = {
             "~": self.topic,
             "name": self.name,
-            "unique_id": self.unique_id,
-            "stat_t": "~/state",
+            "state_topic": "~/state",
             "unit_of_measurement": self.unit_of_measurement,
             "icon": self.icon,
+            "device": self.parent_device,
         }
-        self.client.publish(f"{self.topic}/config", json.dumps(_config))
+
+        if self.icon:
+            _config["icon"] = self.icon
+
+        mqtt_mi = self.client.publish(
+            f"{DISCOVERY_PREFIX}/{self.component}/{self.parent_device.name}/{self.object_id}/config",
+            json.dumps(_config),
+        )
+        mqtt_mi.wait_for_publish()
 
     def send(self, value):
         self.client.publish(f"{self.topic}/state", str(value))
@@ -54,7 +81,7 @@ class Tracker:
         self.client = client
         self.name = name
         self.unique_id = self.name.replace(" ", "_").lower()
-        self.topic = f"homeassistant/device_tracker/{self.unique_id}"
+        self.topic = f"{DISCOVERY_PREFIX}/device_tracker/{self.unique_id}"
         self._send_config()
 
     def _send_config(self):
@@ -83,7 +110,7 @@ class Binary:
         self.client = client
         self.name = name
         self.unique_id = self.name.replace(" ", "_").lower()
-        self.topic = f"homeassistant/binary_sensor/{self.unique_id}"
+        self.topic = f"{DISCOVERY_PREFIX}/binary_sensor/{self.unique_id}"
         self.icon = icon
         self._send_config()
 
