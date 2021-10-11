323e88ba9c93b72d4f6abd01d21c926bc23f7728
Francois Gervais <francoisgervais@gmail.com>
2021-10-11 11:17:11 -0400

    Add identifiers to device

--- ha_mqtt_device.py
+++ ha_mqtt_device.py
@@ -8,11 +8,12 @@ DISCOVERY_PREFIX = "homeassistant"
 
 
 class Device(dict):
-    def __init__(self, name, sw_version, model, manufacturer):
+    def __init__(self, identifiers, name, sw_version, model, manufacturer):
         super().__init__()
 
         self.name = name
 
+        self["identifiers"] = identifiers
         self["name"] = name
         self["sw_version"] = sw_version
         self["model"] = model
